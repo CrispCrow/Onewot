092aec8e27cdd10ee8bce1aec81f157626116548
CrispCrow <75481894+CrispCrow@users.noreply.github.com>
2022-07-18 21:53:47 +0300

    update client.py

--- wotbwrapper/client.py
+++ wotbwrapper/client.py
@@ -1,3 +1,9 @@
+from .user import User
+
+
 class WotbClient:
     def __init__(self, application_id: str) -> None:
         self.application_id = application_id
+    
+    def get_user(username: str) -> User:
+        ...
