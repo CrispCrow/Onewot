ff44c6f395310da0beb0d824e921c490a23a842a
Denis0529 <denismix006@gmail.com>
2022-07-18 19:55:21 +0300

    Initial commit

---
+++ examples/get_user.py
@@ -0,0 +1,10 @@
+from typing import NamedTuple
+
+from wotbwrapper import WotbClient
+from wotbwrapper.User import User
+
+wotbclient = WotbClient(application_id='anything')
+
+def get_user_info(username: str) -> User:
+	user = wotbclient.get_user('test')
+	return user
---
+++ wotbwrapper/user.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, TypeVar, Generic
+
+__all__ = ('User')
+
+T = TypeVar('T')
+
+class Timestamp(Generic[T]):
+	pass
+
+
+class BaseUser:
+	__slots__ = (
+		'id',
+		'clan_id',
+		'client_language',
+		'created_at',
+		'global_rating',
+		'last_battle_time',
+		'logout_at',
+		'nickname',
+		'updated_at',
+	)
+
+	if TYPE_CHECKING:
+		id: int
+		clan_id: int
+		client_language: str
+		created_at: Timestamp[datetime.datetime]
+		global_rating: int
+		last_battle_time: Timestamp[datetime.datetime]
+		logout_at: Timestamp[datetime.datetime]
+		nickname: str
+		updated_at: Timestamp[datetime.datetime]
+
+
+class User(BaseUser):
+	pass
